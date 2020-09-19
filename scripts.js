428409ef5edc5d640f759d8231ca4f614fbb23a2
Radiowire <radiowire@users.noreply.github.com>
2020-09-19 22:19:32 +0200

    update css, added layout button
    updated cards, grayscale button, navbar, search bar
    updated css
    added layout button

--- scripts.js
+++ scripts.js
@@ -1,16 +1,41 @@
 $(document).ready(function(){
   
-  $(".btn").click(function(){
+  $(".btn2").click(function(){
     
-    if ( $(".btn").text() == "Black and white" ) {
-      $(".btn").text("Colorful");
+    if ( $(".btn2").text() == "Black and white" ) {
+      $(".btn2").text("Colorful");
     } else {
-      $(".btn").text("Black and white");
+      $(".btn2").text("Black and white");
     }
     
     $("img").toggleClass("filter");
     
   });
   
+  
+  $(".btn3").click(function(){
+    
+    
+    $(".card-columns").toggleClass("masonry");
+    
+  });
+  
 });
 
+$("switch-input").click(function(){
+    $("img").toggleClass("filter");
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+  var checkbox = document.querySelector('input[type="checkbox"]');
+
+  checkbox.addEventListener('change', function () {
+    if (checkbox.checked) {
+      $("img").toggleClass("filter")
+      
+    } else {
+      $("img").toggleClass("filter-off");
+      
+    }
+  });
+});
